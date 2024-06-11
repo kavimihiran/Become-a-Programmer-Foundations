2387f0a829215a918702101161c342e219cb8f9b
kavimihiran <kavindujagoda@gmail.com>
2024-06-11 19:17:12 +0530

    completed the course beyond the fundamentals and the excersis associated with that course

---
+++ Day 2 beyond programming fundamentals/Ch02/02_04/begin/for_loop_challenge.py
@@ -0,0 +1,12 @@
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+print("our fruit selection")
+for fruit in fruits:
+    print(fruit)
+
---
+++ Day 2 beyond programming fundamentals/Ch04/04_05/begin/app.py
@@ -0,0 +1,5 @@
+miles = input('Enter a distance in miles: ')
+
+kilometers_value = float(miles) * 1.609344
+
+print("Value in kilometers " + str(kilometers_value))
